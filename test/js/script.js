3f2aa3553e5eb158227739e3934bd140ef7dde0b
phuong2414 <98210417+phuong2414@users.noreply.github.com>
2022-09-22 22:48:37 +0700

    js
    js

--- test/js/script.js
+++ test/js/script.js
@@ -1,9 +1,9 @@
 var swiper = new Swiper(".slide-content", {
-  slidesPerView: 1,
+  slidesPerView: 3,
   spaceBetween: 25,
-  loopFillGroupWithBlank: true,
+  loop: true,
+  fade: true,
   centeredSlides: false,
-  slidesPerGroupSkip: 1,
   grabCursor: true,
   keyboard: {
     enabled: true,
